5ae12acac7f6f6f9f4fd80b8a4328f23bac653ff
Nikki Holland-Plum <mnhollandplum@gmail.com>
2022-01-10 15:33:18 -0700

    Complete todo controller to pass tests

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,30 @@ class TodosController < ApplicationController
     json_response(@todos)
   end
 
+  def create
+    @todo = Todo.create!(todo_params)
+    json_response(@todo, :created)
+  end
+
+  def show
+    json_response(@todo)
+  end
+
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  def todo_params
+    params.permit(:title, :created_by)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 end

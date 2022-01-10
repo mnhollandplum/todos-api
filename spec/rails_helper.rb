c77670427b099ece748e678151c923c4f9408be7
Nikki Holland-Plum <mnhollandplum@gmail.com>
2022-01-10 14:25:43 -0700

    Update spec for requests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,7 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -41,6 +42,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.include RequestSpecHelper, type: :request
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your

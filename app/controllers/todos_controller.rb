e8a9fb6734ef6580836f12459dd62b518d8bbcdc
Nikki Holland-Plum <mnhollandplum@gmail.com>
2022-01-12 11:33:11 -0700

    Update auth, item and todos to connect with user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,12 +2,12 @@ class TodosController < ApplicationController
   before_action :set_todo, only: [:show, :update, :destroy]
 
   def index
-    @todos = Todo.all
+    @todos = current_user.todos
     json_response(@todos)
   end
 
   def create
-    @todo = Todo.create!(todo_params)
+    @todo = current_user.todos.create!(todo_params)
     json_response(@todo, :created)
   end
 
@@ -26,7 +26,7 @@ class TodosController < ApplicationController
   end
 
   def todo_params
-    params.permit(:title, :created_by)
+    params.permit(:title)
   end
 
   def set_todo
